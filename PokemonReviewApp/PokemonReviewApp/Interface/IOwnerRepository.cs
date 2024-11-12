3f2467ca02ca47be262c691092f7189ce3c0bc1f
Collier-A-B <benjaminac46@gmail.com>
2024-11-12 16:33:44 -0500

    Tutorial Part 8 Incomplete

---
+++ PokemonReviewApp/PokemonReviewApp/Interface/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.models;
+
+namespace PokemonReviewApp.Interface
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerOfPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+    }
+}
---
+++ PokemonReviewApp/PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,28 @@
+﻿using PokemonReviewApp.Interface;
+using PokemonReviewApp.models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
